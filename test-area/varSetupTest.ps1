d5224313128eceeb98d44c5cc499b8e3c6f93257
makeway4pK <makeway4pK@gmail.com>
2023-04-26 15:01:07 +0530

    var objs investigated

--- test-area/varSetupTest.ps1
+++ test-area/varSetupTest.ps1
@@ -1,13 +1,16 @@
 # $MyInvocation
 $name = (gi $PSCommandPath).BaseName
-if ($name -match '\d') {
+if ($name -match '\d+') {
 	$num = [int]$matches[0]
 	if ($num -ne 1) {
-		&($MyInvocation.InvocationName -replace $num, ($num - 1))
- }
+		iex ($MyInvocation.InvocationName -replace $num, ($num - 1))
+	}
 	else {
-		&($MyInvocation.InvocationName -replace '\d.', '' )
+		iex ($MyInvocation.InvocationName -replace '\d+.', '' )
 	}
 }
-else { $num++ }
+else { $num = 0 }
 sv "varFrom$num"  "string was set in $name"
+"varFrom$num`: " + (iex "echo 'varFrom$num'")
+
+# $n=100;$content=gc .\test-area\varSetupTest.ps1;1..$n|%{$content|sc "./test-area/varSetupTest.$_.ps1"};iex ".\test-area\varSetupTest.$n.ps1";1..$n|%{ri "./test-area/varSetupTest.$_.ps1"}
