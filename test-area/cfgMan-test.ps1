47dd79d9099e23062194651dae954bcc43c86990
makeway4pK <makeway4pK@gmail.com>
2023-04-28 00:17:43 +0530

    coded var list cleanup

--- test-area/cfgMan-test.ps1
+++ test-area/cfgMan-test.ps1
@@ -1,25 +1,35 @@
 param(
-	[string[]] $get
+	[string[]] $setVars,
+	[string] $prefix
 )
 
 function Get-Vars([string[]] $varList) {
 	
 }
 
-function set-vars([string[]]) {}
+function Set-Vars([string[]] $toSetVarList) {}
 function Update-Roll() {
 	$newroll = get-roll+Find-newvars
 	Check-nonnull $newroll
 }
-function get-Roll() {
-	return $roll = ./ cfgroll 
+function Get-Roll() {
+	return $roll = ./cfgRoll.ps1
 }
-function Find-newvars() {
+function Find-NewVars() {
 	return [string[]] $ListOfnewVarNames
 }
-function Check-nonnull([string[]]$ListtoValidate) {
+function Check-NonNull([string[]]$ListtoValidate) {
 	return [bool]$yesORno 
 }  
-function filter-null([string[]]$ListtoFilter) {
+function Filter-Null([string[]]$ListtoFilter) {
 	return [string[]]$nonnull
+}
+
+$toSetVarList = ($setVars -replace '[^\s\w\d_]'
+).trim() -replace ' ', '_'  | ? {
+	$_.length -gt 0
+}
+
+if ($toSetVarList) {
+	Get-Vars $toSetVarList
 }
