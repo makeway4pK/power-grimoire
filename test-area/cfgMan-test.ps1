a3f805d6b3069df4668ffbe4fb64dd4b890c369a
makeway4pK <makeway4pK@gmail.com>
2023-05-16 21:25:17 +0530

    coded var list cleanup

--- test-area/cfgMan-test.ps1
+++ test-area/cfgMan-test.ps1
@@ -2,14 +2,31 @@ param(
 	[string[]] $setVars,
 	[string] $prefix
 )
-
+function Make-Roll {
+	# init cfgRoll from cfgRef
+}
+function Sync-Roll {
+ #returns false for no probs, else exit msg
+	if (!(Test-Path ./cfgRoll.ps1)) {
+		return Make-Roll
+	}
+	$rollFile = gi ./cfgRoll.ps1
+	try {
+		$lastUpdate = gc ./cfgBox/cfgRollLastUpdated.txt
+	}
+	catch {}
+	if ($rollFile.LastWriteTimeUtc.ToString() -ne $lastUpdate) {
+		return Update-Roll
+	}
+	return $false
+}
 function Get-Vars([string[]] $varList) {
 	
 }
 
 function Set-Vars([string[]] $toSetVarList) {}
 function Update-Roll() {
-	$newroll = get-roll+Find-newvars
+	$newroll = get-roll + Find-newvars
 	Check-nonnull $newroll
 }
 function Get-Roll() {
@@ -25,11 +42,17 @@ function Filter-Null([string[]]$ListtoFilter) {
 	return [string[]]$nonnull
 }
 
-$toSetVarList = ($setVars -replace '[^\s\w\d_]'
-).trim() -replace ' ', '_'  | ? {
-	$_.length -gt 0
-}
+$toSetVarList = (($setVars -replace '[^\s\w\d_]'
+	).trim() | ? {
+		$_.length -gt 0
+	}) -replace ' ', '_'
 
 if ($toSetVarList) {
 	Get-Vars $toSetVarList
+}
+elseif (!($out = Sync-Roll)) {
+	"`n No updates to cfgRoll were detected.`n"
+}
+else {
+	$out
 }
