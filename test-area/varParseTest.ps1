3b4c9dd7db8e211fa60851fbdc9ec54cca448614
makeway4pK <makeway4pK@gmail.com>
2023-05-17 18:42:47 +0530

    parser speed test results

--- test-area/varParseTest.ps1
+++ test-area/varParseTest.ps1
@@ -1,3 +1,12 @@
+<#
+.synopsis
+tester for variable set
+.description
+rough script only
+.notes
+nothing much
+#>
+
 # return ./test-area/test.ps1
 $rable = ./test-area/test.ps1
 foreach ($thing in $rable.keys) {
