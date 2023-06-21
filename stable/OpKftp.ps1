4d8fc7680d94052524514389ac8e07d3b221c6bd
makeway4pK <makeway4pK@gmail.com>
2023-06-21 22:33:42 +0530

    pipe to sls to avoid flat input

--- stable/OpKftp.ps1
+++ stable/OpKftp.ps1
@@ -2,7 +2,7 @@
 . ./cfgMan.ps1 -get 'ftp_user', 'ftp_pass', 'ftp_port'
 
 $arpout = arp -a
-$slsout = sls -InputObject $arpout -Pattern '^.*?(((25[0-5]|(2[0-4]|1?\d)?\d)\.?\b){4}).*?dynamic.*?$' -AllMatches
+$slsout = $arpout | sls  -Pattern '^.*?(((25[0-5]|(2[0-4]|1?\d)?\d)\.?\b){4}).*?dynamic.*?$' -AllMatches
 foreach ($line in $slsout) {
 	foreach ($match in $line.matches) {
 		explorer "ftp://${ftp_user}:${ftp_pass}@"+$match.groups[1].value+":${ftp_port}" 
