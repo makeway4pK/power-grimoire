f5ec6a09e5de6d1e4154ace8661c534c3c4ba145
makeway4pK <makeway4pK@gmail.com>
2023-06-21 21:36:42 +0530

    cfgman call is case sensitive

--- stable/OpKftp.ps1
+++ stable/OpKftp.ps1
@@ -1,4 +1,4 @@
 
-. ./cfgman.ps1 -get 'ftp_user', 'ftp_pass', 'ftp_port'
+. ./cfgMan.ps1 -get 'ftp_user', 'ftp_pass', 'ftp_port'
 
 arp -a | sls '.*?(((25[0-5]|(2[0-4]|1?\d)?\d)\.?\b){4}).*?dynamic.*?'-AllMatches | % { $_.matches | % { explorer "ftp://${ftp_user}:${ftp_pass}@"+$_.groups[1].value+":${ftp_port}" } }
