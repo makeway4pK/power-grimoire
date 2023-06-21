da149939ac2b558b0da811281090d6c29e350405
makeway4pK <makeway4pK@gmail.com>
2023-06-21 22:34:17 +0530

    needed parentheses

--- stable/OpKftp.ps1
+++ stable/OpKftp.ps1
@@ -5,6 +5,6 @@ $arpout = arp -a
 $slsout = $arpout | sls  -Pattern '^.*?(((25[0-5]|(2[0-4]|1?\d)?\d)\.?\b){4}).*?dynamic.*?$' -AllMatches
 foreach ($line in $slsout) {
 	foreach ($match in $line.matches) {
-		explorer "ftp://${ftp_user}:${ftp_pass}@"+$match.groups[1].value+":${ftp_port}" 
+		explorer("ftp://${ftp_user}:${ftp_pass}@" + $match.groups[1].value + ":${ftp_port}")
 	}
 }
