371706e03fbe360f4c809da9340ce1a7301df9fd
makeway4pK <makeway4pK@gmail.com>
2023-06-29 15:35:17 +0530

    args check

--- test-area/CompareCode.ps1
+++ test-area/CompareCode.ps1
@@ -1,2 +1,10 @@
+param(
+	[Switch]$sw
+)
+if ($true) {
+	'' | sc ([system.environment]::getfolderpath('desktop') + '/file.txt')
+	$args -join ' ' | % { $_ | ac ([system.environment]::getfolderpath('desktop') + '/file.txt') }
+	return
+}
 code --diff $args[1] $args[0]
 if (!$?) { Read-host }
