d34bf4d85c1a27b21a1113aa0d882c97ff446acd
makeway4pK <makeway4pK@gmail.com>
2023-06-29 17:10:06 +0530

    coded deployer

--- test-area/CompareCode.ps1
+++ test-area/CompareCode.ps1
@@ -1,6 +1,29 @@
 param(
-	[Switch]$sw
+	[Switch]$Deploy
 )
+if ($Deploy) {
+	$VScodeXe = (gci($env:path -split ';' -match 'VS Code' -replace 'bin') | ? -Property Name -eq 'Code.exe').FullName
+	if (!$VScodeXe) {
+		Write-Error -Message "`n`nVS code executable not found!`n Make sure VS code is installed and `n available in the path variable before trying again" -Category ResourceUnavailable
+		return
+	}
+	
+	[string]$pwsh = Resolve-Path "$PSScriptRoot/../modules/bin/powershellw.exe"
+	if (!$pwsh) {
+		$pwsh = 'powershell' 
+	}
+	
+	$ScriptPath = "$PSScriptRoot/" + $MyInvocation.MyCommand
+	$Shortcut = [Environment]::GetFolderPath('SendTo') + '/Compare Code.lnk'
+	$s = (New-Object -ComObject wscript.shell).CreateShortcut($Shortcut)
+	if (!$?) { return }
+	$s.TargetPath = $pwsh
+	$s.IconLocation = $VScodeXe
+	$s.Arguments = $ScriptPath + ' -Compare'
+	$s.WindowStyle = 7 # 1, 3, 7 = Normal, Maximized, Minimized
+	$s.Save()
+	return
+}
 if ($true) {
 	'' | sc ([system.environment]::getfolderpath('desktop') + '/file.txt')
 	$args -join ' ' | % { $_ | ac ([system.environment]::getfolderpath('desktop') + '/file.txt') }
