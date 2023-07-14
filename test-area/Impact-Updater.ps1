2986cfb42de29eba2a6d5b6a537cc252ef0bdebc
makeway4pK <makeway4pK@gmail.com>
2023-07-15 00:18:01 +0530

    impact-updater: tune up

--- test-area/Impact-Updater.ps1
+++ test-area/Impact-Updater.ps1
@@ -1,10 +1,11 @@
 #requires -version 3.0
-#####equires -RunAsAdministrator
+#requires -RunAsAdministrator
 
 . ./cfgMan.ps1 -get 'impact_path'
 
-$delay = 15
+$FocusDelay = 20
+$FocusAt = @(1160, 670)
+. ./stable/LaunchIf.ps1 $impact_path -Admin -Online -Charging -Focus launcher -FocusAt $FocusAt -FocusDelay $FocusDelay
 
-$impact_path
-'sadc'
-# ./stable/LaunchIf.ps1 -Admin -Online -Charging -Focus launcher, 1170, 700 -Launch $impact_path
+sleep 5
+[Clicker]::LeftClickAtPoint($FocusAt[0], $FocusAt[1])
