b34b7af371c78d845256e07f379f090daf1edabc
makeway4pK <makeway4pK@gmail.com>
2023-08-11 00:58:57 +0530

    steam launch tested and tuned
    steam full update not yet tested

--- test-area/Steam-Silent.ps1
+++ test-area/Steam-Silent.ps1
@@ -2,34 +2,37 @@ param(
 	[string] $match_name
 )
 . ./cfgMan.ps1 -get steam_path
+$proc_wait = 20
+$max_wait = 600
+$wind_wait = 2
+
+$steam_path += "/steam.exe"
+$proc_name = 'steamwebhelper'
 # if (!$match_name) { exit }
 
-#Check if Steam is already running launch and minimize if not running
-if (!(Get-Process -ErrorAction Ignore steam)) {
+# if not running, launch and minimize Steam
+if (!(Get-Process -ErrorAction Ignore $proc_name)) {
 	Start-Process $steam_path
+	if (!$?) { exit } # if launch failed
+	
 	$wh = ./stable/addtype-WindowHandler.ps1
-	$timeout = $max_wait * 2
 	# wait for process and window handle
-	while (!($hnd = (Get-Process -ErrorAction Ignore steam).where({ $_.MainWindowTitle }).MainWindowHandle) -and
-		$timeout--) { sleep -Milliseconds 500 }
-		
-	# high freq finite wait for window, in case of quick launch
-	$timeout *= 5
-	while ( $hnd -ne $wh::GetForegroundWindow() -and $timeout--) {
-		sleep -Milliseconds 100
+	$timeout = $proc_wait * 2
+	while (!($hnd = (Get-Process -ErrorAction Ignore $proc_name).where(
+				{ $_.MainWindowTitle -eq 'Steam' } # Avoids interrupting update dialog
+			).MainWindowHandle) -and $timeout--)
+	{ Start-Sleep -Milliseconds 500 }
+	$timeout = $max_wait
+	while (!($hnd = (Get-Process -ErrorAction Ignore $proc_name).where(
+				{ $_.MainWindowTitle -eq 'Steam' } # Avoids interrupting update dialog
+			).MainWindowHandle) -and $timeout--)
+	{ Start-Sleep 1 }
+	
+	# Watch new windows and hide them quickly
+	$timeout = $wind_wait * 10
+	while ($timeout--) {
+		Start-Sleep -Milliseconds 100
+		# Hide window, needs admin
+		$wh::ShowWindow($hnd, 0)
 	}
-	# low freq indefinite wait for window, in case of update found
-	while ( $hnd -ne $wh::GetForegroundWindow()) {
-		# abort if steam process is lost for some reason
-		if (!(Get-Process -ErrorAction Ignore steam)) { exit }
-		sleep 1
-	}
-	if (!(Get-Process -ErrorAction Ignore steam)) { exit }
-	
-	# Hide window, needs admin
-	# $wh::ShowWindow($hnd, 0)
-	"handle Hidden" #dbg
-	
-	
-	
 }
