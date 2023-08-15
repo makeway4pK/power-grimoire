a3d181c521a9a290d458110c3b7182b97d5ed399
makeway4pK <makeway4pK@gmail.com>
2023-08-16 00:17:18 +0530

    remote branches clause, not tested

--- test-area/Git-DelMergedB.ps1
+++ test-area/Git-DelMergedB.ps1
@@ -13,10 +13,21 @@ $excludedBranches_matchPattern = '(main|dev|develop|master)'
 # local branches first
 
 $localBranches = (git branch --merged $mainBranch).trim().where(
-	{ $_ -notmatch $excludedBranches_matchPattern }
-)
+	{ $_ -notmatch $excludedBranches_matchPattern } )
 "These are the local branches that will be deleted:"
 $confirm = Read-Host
-if ($confirm -match '(Y|y)') {
+"Continue? (Y/n)"
+if ($Host.UI.RawUI.ReadKey().Character -match '(Y|y)') {
 	$localBranches | ForEach-Object { git branch -d $_ }
+}
+
+$remoteBranches = (git branch -r --merged $mainBranch
+) | ForEach-Object { , ($_.trim() -split '/')
+} | Where-Object { $_[1] -notmatch '(main|dev|develop|master)' }
+"These are the remote branches that will be deleted:"
+$remoteBranches
+"Continue? (Y/n)"
+if ($Host.UI.RawUI.ReadKey().Character -match '(Y|y)') {
+	# $localBranches | ForEach-Object { git push --delete $_[0] $_[1] }
+	# Can't test without a merged branch, stay tuned
 }
