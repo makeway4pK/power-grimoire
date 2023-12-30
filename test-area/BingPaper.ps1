266f1f7f10760dfce977b0868a1c1aac2d22a90f
makeway4pK <makeway4pK@gmail.com>
2023-12-30 15:51:20 +0530

    bingpaper: copied base from picsum

--- test-area/BingPaper.ps1
+++ test-area/BingPaper.ps1
@@ -1,4 +1,41 @@
+# Created: 30Dec2023 1pm
+# @makeway4pK
+# This script changes the desktop and lockscreen wallpaper, sourced from Bing's
+# daily images.
 
+. ./cfgMan.ps1 -get 'BingPaper_saveLoc'
+
+$oldSaveLocation = "${BingPaper_saveLoc}-old"
+$retryDelay = 30
+
+function BingPaper {
+	if (!(Test-Path -Path $BingPaper_saveLoc)) {
+		if (!(Test-Path -Path $oldSaveLocation)) {
+			Get-Wallpaper
+		}
+		else {
+			while (!(Test-Path -Path $BingPaper_saveLoc)) { Start-Sleep -Seconds 2 }
+		}
+	}
+	
+	Apply-Wallpaper
+	Apply-LockPaper
+	Mark-AsOld
+	Get-BingPaper
+}
+
+function Mark-AsOld { Rename-Item $BingPaper_saveLoc $oldSaveLocation }
+
+function Apply-LockPaper {
+	$RegKeyPath = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\PersonalizationCSP'
+	New-Item -Path $RegKeyPath -Force
+	New-ItemProperty -Path $RegKeyPath -Name 'LockScreenImagePath' -Value $BingPaper_saveLoc -PropertyType STRING -Force
+}
+function Apply-Wallpaper {
+	Push-Location $PSScriptRoot
+	./Set-Wallpaper.ps1 -Path $picsumpaper_saveLoc -Style Span
+	Pop-Location 
+}
 function Get-BingPaper {
 	$mkt_str = '' # one of 'en-US', 'en-UK', '', etc.
 	$o = $null
@@ -7,7 +44,9 @@ function Get-BingPaper {
 	}while (!$? -and !(Start-Sleep -Seconds $retryDelay))
 	$url = $o.images[0].url
 	do {
-		$o = Invoke-WebRequest "http://www.bing.com/$url" -OutFile $picsumpaper_saveLoc
+		$o = Invoke-WebRequest "http://www.bing.com/$url" -OutFile $BingPaper_saveLoc
 	}while (!$? -and !(Start-Sleep -Seconds $retryDelay))
 	Remove-Item $oldSaveLocation
 }
+
+BingPaper
