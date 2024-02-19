e032cbf7436d37ac6bdc737b2b455de7a721ca0f
makeway4pK <makeway4pK@gmail.com>
2024-02-19 11:38:24 +0530

    bingtester: added file op to testmaker

--- test-area/Bingpaper-testmaker.ps1
+++ test-area/Bingpaper-testmaker.ps1
@@ -3,13 +3,20 @@
 # Makes a duplicate skipmemory for bingpaper.
 # Takes a list of integers and converts to dates
 # relative to today for the skipmem.
-
+. ./cfgMan.ps1 -get 'BingPaper_saveLoc'
+$bingDateFormat = 'yyyyMMdd'
 $list = @()
-'Enter list of ints to test BingPaper, x to stop:'
+'Enter an odd number of strictly increasing ints to test BingPaper, x to stop:'
 $i = [int](Read-Host)
 while ($?) {
 	$list += $i
 	$i = [int](Read-Host)
 }
+
+for ($i = 0; $i -lt $list.Count; $i++) {
+	$list[$i] = [datetime]::Today.AddDays(-$list[$i]).ToString($bingDateFormat)
+}
+
+"`n" + ($list -join '') | Add-Content $BingPaper_saveLoc
 
 
